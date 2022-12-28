753ce2fcf450cbb0701f582a3000f5add675f473
DESKTOP-VU6L4E9\Abdul <beridzegiorgi001@gmail.com>
2022-12-28 23:20:47 +0400

    js ცვლადების შექმნა და მათემატიკური მოქმედებები

---
+++ asseto/scrips.js
@@ -0,0 +1,21 @@
+let string = "hay";
+let number = 10;
+let boolean = true;
+let boolean2 = false;
+
+console.log(typeof 20);
+console.log(typeof true);
+console.log(typeof "hay");
+
+const x = 20, y = 10;
+console.log( x+y );
+console.log( x-y );
+console.log( x/y );
+console.log( x*y );
+console.log( x%y );
+
+if (( x%2) > 0) {console.log('რიცხვი კენტია');
+    
+} else {
+    console.log('რტიცხვი ლუწია');
+}
